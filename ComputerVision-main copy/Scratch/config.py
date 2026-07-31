f98d42d4fb8e28059954eb6ad21b8f60fcaa336b
Harshith Narayanarao Shankarananda <harshithplays6112@gmail.com>
2026-07-31 17:22:50 +1000

    created a config file for all the settings

---
+++ ComputerVision-main copy/Scratch/config.py
@@ -0,0 +1,18 @@
+import os
+
+# Folder that contains "splits/" and "subset/" (one level above Scratch/).
+SCRIPT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(SCRIPT_FOLDER)
+
+NUM_CLASSES = 500
+
+BATCH_SIZE = 32
+NUM_WORKERS = 2
+
+NUM_EPOCHS = 30
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+WEIGHT_DECAY = 5e-4
+
+CHECKPOINT_DIR = os.path.join(SCRIPT_FOLDER, "checkpoints")
+BEST_CHECKPOINT_PATH = os.path.join(CHECKPOINT_DIR, "resnet18_scratch_best.pth")
